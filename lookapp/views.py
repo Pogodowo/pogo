2a288277f6b91acd37b43a9cd35638cfc28b581d
Luk <lukabr@op.pl>
2020-01-09 17:10:19 +0100

    przeniesienie if z home do vievs

--- lookapp/views.py
+++ lookapp/views.py
@@ -2,6 +2,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"home.html",{})
+    import json
+    import requests
+
+    try:
+        api_req = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Warsaw&APPID=6eccd0c1abcd929aca44f63dd104bfaf")
+        api = json.loads(api_req.content)
+        #temp=api['main']['Temp']
+        #temp= int(temp)
+        #temp=temp-273
+    except exception as e:
+        e = "error"
+
+    return render(request,"home.html",{'api':api})
 def O_mnie(request):
     return render(request,"O_mnie.html",{})
